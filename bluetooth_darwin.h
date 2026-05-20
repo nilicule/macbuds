c410cbc0f128bba62f14aa6164d24b23a367f3a4
Remco Brink <nilicule@gmail.com>
2026-05-20 10:25:06 +0200

    Add C header for native Bluetooth bridge

---
+++ bluetooth_darwin.h
@@ -0,0 +1,25 @@
+#ifndef MACBUDS_BLUETOOTH_DARWIN_H
+#define MACBUDS_BLUETOOTH_DARWIN_H
+
+#define BT_MAX_DEVICES 64
+
+typedef struct {
+    char mac[32];
+    char name[256];
+} bt_device_t;
+
+typedef enum {
+    BT_EVENT_CONNECTED = 1,
+    BT_EVENT_DISCONNECTED = 2,
+    BT_EVENT_CONNECT_FAILED = 3
+} bt_event_kind_t;
+
+int  bt_paired_devices(bt_device_t *out, int max_count);
+int  bt_is_connected(const char *mac);
+int  bt_connect(const char *mac);
+int  bt_disconnect(const char *mac);
+
+int  bt_start_monitoring(const char *mac);
+void bt_stop_monitoring(void);
+
+#endif
