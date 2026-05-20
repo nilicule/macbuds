e1b4471ef4a5e397e9efcde5826caee7dd74fe6c
Remco Brink <nilicule@gmail.com>
2026-05-20 10:25:06 +0200

    Add Go cgo wrapper for native Bluetooth bridge

---
+++ bluetooth_darwin.go
@@ -0,0 +1,100 @@
+package main
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework IOBluetooth -framework Foundation
+#include <stdlib.h>
+#include "bluetooth_darwin.h"
+*/
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type BluetoothEventKind int
+
+const (
+	BluetoothConnected     BluetoothEventKind = C.BT_EVENT_CONNECTED
+	BluetoothDisconnected  BluetoothEventKind = C.BT_EVENT_DISCONNECTED
+	BluetoothConnectFailed BluetoothEventKind = C.BT_EVENT_CONNECT_FAILED
+)
+
+type BluetoothEvent struct {
+	Kind BluetoothEventKind
+	MAC  string
+}
+
+var btEvents = make(chan BluetoothEvent, 16)
+
+func BluetoothEvents() <-chan BluetoothEvent { return btEvents }
+
+//export goOnBluetoothEvent
+func goOnBluetoothEvent(kind C.int, mac *C.char) {
+	ev := BluetoothEvent{
+		Kind: BluetoothEventKind(kind),
+		MAC:  C.GoString(mac),
+	}
+	select {
+	case btEvents <- ev:
+	default:
+	}
+}
+
+func PairedDevices() ([]BluetoothDevice, error) {
+	var buf [C.BT_MAX_DEVICES]C.bt_device_t
+	n := int(C.bt_paired_devices(&buf[0], C.int(len(buf))))
+	if n < 0 {
+		return nil, fmt.Errorf("bt_paired_devices failed")
+	}
+	out := make([]BluetoothDevice, 0, n)
+	for i := 0; i < n; i++ {
+		out = append(out, BluetoothDevice{
+			Address: C.GoString(&buf[i].mac[0]),
+			Name:    C.GoString(&buf[i].name[0]),
+		})
+	}
+	return out, nil
+}
+
+func IsConnected(mac string) (bool, error) {
+	cmac := C.CString(mac)
+	defer C.free(unsafe.Pointer(cmac))
+	r := int(C.bt_is_connected(cmac))
+	if r < 0 {
+		return false, fmt.Errorf("bt_is_connected failed")
+	}
+	return r == 1, nil
+}
+
+func Connect(mac string) error {
+	cmac := C.CString(mac)
+	defer C.free(unsafe.Pointer(cmac))
+	if int(C.bt_connect(cmac)) != 0 {
+		return fmt.Errorf("bt_connect failed")
+	}
+	return nil
+}
+
+func Disconnect(mac string) error {
+	cmac := C.CString(mac)
+	defer C.free(unsafe.Pointer(cmac))
+	if int(C.bt_disconnect(cmac)) != 0 {
+		return fmt.Errorf("bt_disconnect failed")
+	}
+	return nil
+}
+
+func StartMonitoring(mac string) error {
+	cmac := C.CString(mac)
+	defer C.free(unsafe.Pointer(cmac))
+	if int(C.bt_start_monitoring(cmac)) != 0 {
+		return fmt.Errorf("bt_start_monitoring failed")
+	}
+	return nil
+}
+
+func StopMonitoring() {
+	C.bt_stop_monitoring()
+}
